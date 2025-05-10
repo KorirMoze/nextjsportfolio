025843e915a9527e8d6a4521560b6a550d9a2497
Njoki Mwai <mercyneemam@gmail.com>
2025-05-10 13:12:21 +0300

    Portfolio creation (#4)
    * initial commit
    
    * commit message

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import "../styles/globals.css";
+
+export const metadata = {
+    title: "Korir's Portfolio",
+    description: "Welcome to my portfolio!",
+  };
+  
+  export default function RootLayout({ children }) {
+    return (
+      <html lang="en">
+        <body>
+
+          <main>
+          {children}
+          </main>
+          
+        </body>
+      </html>
+    );
+  }
