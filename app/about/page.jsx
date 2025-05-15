60070046095ce2cd620a11cf09699f0115d5621f
mercy-mwai <mercyneemam@gmail.com>
2025-05-15 12:57:12 +0300

    commit message

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -8,7 +8,17 @@ const AboutSection = () => {
         <h1 className='text-3xl sm:text-4xl font-bold mb-3 pt-5 text-black-600'>About Me</h1>
         <p className='text-gray-400 '>Introduction</p>
       </div>
+      <div className='max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-start gap-12'>
+        {/*left side*/}
+        <div className="w-full md:w-1/2 space-y-8">
+        {/*Image*/}
+        </div>
 
+        {/*right side*/}
+        <div className='max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-start gap-12'>
+          
+        </div>
+      </div>
     </Banner>
   )
 }
