f9a7a9640c5edfc25af5271030b5c6d2435799d6
mercy-mwai <mercyneemam@gmail.com>
2025-05-15 12:50:34 +0300

    commit message

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,8 +1,15 @@
+import Banner from '@/components/Banner'
 import React from 'react'
 
 const AboutSection = () => {
   return (
-    <div>Aboutsection</div>
+    <Banner className="px-6 py-16 md:px-20  ">
+      <div className=" text-center ">
+        <h1 className='text-3xl sm:text-4xl font-bold mb-3 pt-5 text-black-600'>About Me</h1>
+        <p className='text-gray-400 '>Introduction</p>
+      </div>
+
+    </Banner>
   )
 }
 
